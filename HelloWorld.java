fafcd9530fbde08d0830c708d6476c084f792568
dbaitman <dmitry.baitman@gmail.com>
2019-02-24 22:55:13 +0000

    git with remote commit 1

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
    public static void main(String[] args) {
       // Prints "Hello, World" in the terminal window.
-      System.out.println("Hello, World via GIT R ");
+      System.out.println("Hello, World via GIT triger after commit 1 ");
    }
 }
